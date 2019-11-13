f224ae445dab3e20d8950e7e7d9210a0763803fc
RezaMaranatha <rezamaranatha@yahoo.com>
2019-11-13 19:38:09 +0700

    lagi

--- static/js/video.js
+++ static/js/video.js
@@ -25,7 +25,7 @@ function enableVideo() {
 // kickstart the video call.
 function loadSimpleWebRTC() {
   var script = document.createElement("script");
-  script.src = "https://simplewebrtc.com/latest-v3.js";
+  script.src = "/static/js/latest-v3.js";
   document.head.appendChild(script);
 
   script.onload = function() {
