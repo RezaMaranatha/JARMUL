904cbe7b98843e7abf0bf0e4cf83921d206e3b3c
trasv <timotiuswishnubinsarpardede@ymail.com>
2019-11-14 14:20:09 +0700

    Update scrpt.js

--- static/js/scrpt.js
+++ static/js/scrpt.js
@@ -184,4 +184,4 @@
 
     insertMessageToDOM(data, true);
   });
-  insertMessageToDOM({content: 'Paste This to chat and video call' + location.href});
+  insertMessageToDOM({content: 'Paste This to chat and video call  ' + location.href});
